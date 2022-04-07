355f1384a48070ed5815a27b576bee916f004770
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-04-07 19:59:12 +0600

    tut 7 done

--- 3_create_vue3/app.js
+++ 3_create_vue3/app.js
@@ -4,7 +4,18 @@ const app =  Vue.createApp({
         message: 'Hello Vue!',
         title: 'this is a title',
         link:'https://vuejs.org/guide/quick-start.html',
+        html: '<h2> This is HTML  </h2>',
+        name:null,
       }
-    }
+    },
+    methods: {
+        getHtml(){
+            return this.html;
+        },
+        setName(event,arg){
+            this.name = event.target.value + arg;
+            // console.log(event.target);
+        }
+    },
   });
   app.mount('#app');
