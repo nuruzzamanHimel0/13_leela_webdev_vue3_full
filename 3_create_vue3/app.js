0e366b6261076724a2ca211fcc779a78bc7c9843
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-04-08 11:07:33 +0600

    v-if

--- 3_create_vue3/app.js
+++ 3_create_vue3/app.js
@@ -1,9 +1,8 @@
 const app =  Vue.createApp({
     data() {
       return {
-        boxNumberOne:null,
-        boxNumberTwo:null,
-        boxNumberThree:null,
+        hobbies:[],
+        hobbiesInput:'',
       }
     },
     watch:{
@@ -13,30 +12,9 @@ const app =  Vue.createApp({
       
     },
     methods: {
-        boxColorChange(arg){
-            if(arg == 1){
-                if(this.boxNumberOne != null){
-                    this.boxNumberOne = null;
-                }else{
-                    this.boxNumberOne = 1;
-                }
-               
-            }else if(arg == 2){
-                if(this.boxNumberTwo != null){
-                    this.boxNumberTwo = null;
-                }else{
-                    this.boxNumberTwo = 2;
-                }
-          
-            }
-            else if(arg == 3){
-                if(this.boxNumberThree != null){
-                    this.boxNumberThree = null;
-                }else{
-                    this.boxNumberThree = 3;
-                }
-               
-            }
+        submitHobbies(){
+            this.hobbies.push(this.hobbiesInput);
+            this.hobbiesInput = "";
         }
     },
   });
