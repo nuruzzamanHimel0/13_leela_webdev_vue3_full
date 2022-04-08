21819fb44de7b7c8a03aa4f0a3ab1ecbc1c675a3
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-04-08 12:31:47 +0600

    dpme

--- 3_create_vue3/app.js
+++ 3_create_vue3/app.js
@@ -15,6 +15,14 @@ const app =  Vue.createApp({
         submitHobbies(){
             this.hobbies.push(this.hobbiesInput);
             this.hobbiesInput = "";
+        },
+        removeBtn(key){
+            // this.hobbies.splice(key,1);
+            // console.log(key);
+            this.hobbies = this.hobbies.filter(function(value,index,arr){
+                return index !== key;
+            });
+            // console.log(this.hobbies);
         }
     },
   });
