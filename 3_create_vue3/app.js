9dfe26ea61fd03b6005cfe13e0eb298a18e5ae49
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-04-08 13:09:20 +0600

    ref

--- 3_create_vue3/app.js
+++ 3_create_vue3/app.js
@@ -12,18 +12,11 @@ const app =  Vue.createApp({
       
     },
     methods: {
-        submitHobbies(){
-            this.hobbies.push(this.hobbiesInput);
-            this.hobbiesInput = "";
+        submitBtn(){
+            this.hobbiesInput = this.$refs.userText.value;
+        //    console.dir(this.$refs.userText.value);
         },
-        removeBtn(key){
-            // this.hobbies.splice(key,1);
-            // console.log(key);
-            this.hobbies = this.hobbies.filter(function(value,index,arr){
-                return index !== key;
-            });
-            // console.log(this.hobbies);
-        }
+     
     },
   });
   app.mount('#app');
