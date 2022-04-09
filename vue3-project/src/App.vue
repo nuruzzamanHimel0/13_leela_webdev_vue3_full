fd5e165f23ad5b9b9b77998abf654c4b5e6cf619
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-04-09 10:07:10 +0600

    inherite attr

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -4,6 +4,13 @@
       <div class="row">
         <div class="col-md-12">
           <h2>POSTS</h2>
+          <div class="my-4">
+            <compSelectComponet
+              data-status = '1'
+              id="selectBox"
+              @change="selectChange"
+            ></compSelectComponet> 
+          </div>
         
           <div v-for="(value, key) in posts" :key="key">
               <a href="" @click.prevent="changeFirstTitle(key)" >Change First Title here</a>
@@ -21,6 +28,8 @@
 
  import compSInglePost from '@/components/singlePost.vue'
 
+ import compSelectComponet from '@/components/selectComponent.vue'
+
 export default {
   data(){
     return {
@@ -37,11 +46,15 @@ export default {
     }
   },
   components:{
-    compSInglePost
+    compSInglePost,
+    compSelectComponet
   },
   methods:{
     changeFirstTitle(key){
       this.posts[key].title = "This is the change first post title here Number of="+key;
+    },
+    selectChange(event){
+      console.log(event.target.value);
     }
   }
 }
---
+++ vue3-project/src/components/selectComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <select name="" id="" v-bind="$attrs">
+            <option value="">Selected</option>
+            <option value="1">1</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    inheritAttrs:false,
+    data(){
+        return {
+            
+        }
+    },
+    mounted(){
+        console.log(this.$attrs);
+    }
+}
+</script>
