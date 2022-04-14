ca7ba18b23062f44a0b369822fb3fc37d8250f10
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-04-14 09:59:04 +0600

    done

---
+++ create_project_vue3/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Post</h1>
+    <ul>
+      <li v-for="(value, key) in posts" :key="key" >
+        {{ value.title }}
+      </li>
+    </ul>
+    <hr>
+    <single-post></single-post>
+  </div>
+</template>
+
+<script>
+
+import SinglePost from './components/SinglePost.vue'
+
+export default {
+  data(){
+    return {
+      posts:[
+        {
+          title:'fist title',
+          description:'this is the first description'
+        },
+        {
+          title:'secound title',
+          description:'this is the secound description'
+        },
+      ]
+    }
+  },
+  components:{
+    SinglePost
+  }
+
+}
+</script>
---
+++ create_project_vue3/src/components/SinglePost.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>Single Post Component</h2>
+        <ul>
+            <li v-for="(value, key) in post" :key="key" >
+                {{ value.title }}
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            post: [
+            {
+                title:' single post title',
+                description: 'sisngle post descriptn'
+            }
+            ]
+        }
+    }
+}
+</script>
