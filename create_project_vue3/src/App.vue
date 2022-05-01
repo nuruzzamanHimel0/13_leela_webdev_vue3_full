a1c2a48a8673b3ed9fae7e4923a86b7a8c489d27
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-05-01 10:28:40 +0600

    tut 37 slot

--- create_project_vue3/src/App.vue
+++ create_project_vue3/src/App.vue
@@ -1,86 +1,51 @@
 <template>
-  <div>
-    <h1>Post Main Component</h1>
-    <a href="" @click.prevent="changeParentFirstTitle()" >Change Parent Fist Title</a>
-    <ul>
-      <li v-for="(value, key) in posts" :key="key" >
-        {{ value.title }}
-      </li>
-    </ul>
-    <hr>
-    <h2>Tut 34: Provide/Inject:</h2>
-
-    <p>{{ user }}</p>
-    <a href="" class="btn btn-success" @click.prevent="changeUser()" >Change User Name</a>
-    <hr>
-    <div v-for="(value, key) in posts" :key="key" >
-      //componet
-      <single-post :data="value" :isActive="1"
+  <div class="container">
+    <div class="row justify-content-center ">
+      <div class="col-md-6 my-5">
        
-        @changeTitle="onChangeTitle($event)"
-       ></single-post>
+
+        <ButtonComponent >
+          <template v-slot:data>
+             <i class="fa fa-user-circle" aria-hidden="true" ></i> &nbsp; User Data
+          </template>
+          
+        </ButtonComponent>
+        <ButtonComponent >
+            <template v-slot:user>
+             <i class="fa fa-user-circle" aria-hidden="true"></i> &nbsp; User user
+            </template>
+           
+        </ButtonComponent>
+          <ButtonComponent >
+          <template v-slot:user2>
+            <i class="fa fa-user-circle" aria-hidden="true"></i> &nbsp; User user2
+          </template>
+           
+        </ButtonComponent>
+        <hellowWorld></hellowWorld>
+      </div>
     </div>
-  
   </div>
 </template>
 
 <script>
 
-import SinglePost from './components/SinglePost.vue'
-//provide/inject
-import { computed } from 'vue'
-
-export default {
-  data(){
-    return {
-      posts:[
-        {
-          id:1,
-          title:'fist title',
-          description:'this is the first description'
-        },
-        {
-           id:2,
-          title:'secound title',
-          description:'this is the secound description'
-        },
-      ],
-      user:"Nuruzzaman Himel",
-    }
-  },
-  // provide:{
-  //   user:"Nuruzzaman Himel",
-  // },
-  provide(){
-    return {
-        // user:this.user,
-        user: computed(() => this.user),
-        changeUserChild : this.changeUser
-    }
-  },
-  methods:{
-    changeUser(valUser){
-      if(valUser){
-        this.user = valUser;
-      }else{
-        this.user = "Change User name is: Md Nuruzzaman";
+import hellowWorld from '../src/components/HelloWorld.vue'
+import ButtonComponent from './components/ButtonComponent.vue'
+  export default {
+    data(){
+      return {
+        
       }
     },
-    changeParentFirstTitle(){
-      this.posts[0].title ="Change Parent Component Fist Title";
-    },
-    onChangeTitle(event){
-      let index = this.posts.findIndex((post) => post.id == event.id );
-      this.posts[index].title = event.title;
-      this.posts[index].description = event.description;
-        
-      // console.log(index);
-    },
-    
-  },
-  components:{
-    SinglePost
-  }
+    methods:{
+      
 
-}
+    },
+    components:{
+      hellowWorld,
+      ButtonComponent
+    }
+    
+  }
 </script>
--- create_project_vue3/src/services/PostService.js
+++
@@ -1,5 +0,0 @@
-export function Post(id, title, description){
-    this.id = id;
-    this.title = title;
-    this.description = description;
-}
