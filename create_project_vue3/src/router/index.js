cf6426c54728f5619a1c3bfb3db7d67bcea8cc3e
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-05-07 23:19:53 +0600

    before each

--- create_project_vue3/src/router/index.js
+++ create_project_vue3/src/router/index.js
@@ -5,5 +5,15 @@ const router = createRouter({
     routes,
     linkActiveClass:'nav-active'
   });
+  router.beforeEach(async (to, from, next) =>{
+    console.log(to);
+    console.log(from);
+    if(to.path == '/create-user'){
+     
+    }
+    next();
+   
+    //  return false;
+  } );
   
 export default router;
